0f0e64afe2e567d2981965c864dfe173bcb6dd18
LAPTOP-L1T0L126\micha <michalkolarzyk@gmail.com>
2022-11-20 13:31:19 +0100

    Change theme context

--- Domain.Quiz/Quizzes/QuizTheme.cs
+++ Domain.Quiz/Quizzes/QuizTheme.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Quiz.Themes
+namespace Domain.Quiz.Quizzes
 {
-    public class Theme
+    public class QuizTheme
     {
         public Guid Id { get; set; }
         public Color BackgroundColor { get; set; }
